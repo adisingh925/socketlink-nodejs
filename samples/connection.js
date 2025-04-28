2896e20699858a170d6525daea4c8e905a283b65
skillens.ai <skillens.ai@gmail.com>
2025-04-29 02:38:35 +0530

    na

--- samples/connection.js
+++ samples/connection.js
@@ -1,4 +1,4 @@
-const socketlink = require("./socketlink-nodejs");
+const socketlink = require("socketlink-nodejs");
 
 const client = new socketlink({
     clientApiKey: 'sl_d94600b5fefdbc8c03ae63053478ef3a8f338cae4fd6ac3e59bc2fa95d2198e3',
